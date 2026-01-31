32c234c106d713bc3c29b8ee8089cbc69c0b26e6
Maxim Tartachnik <maxim.tartachnik@gmail.com>
2026-01-31 01:07:36 -0500

    downgrade swift syntax

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "Papyrus", targets: ["Papyrus"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax", "601.0.1"..<"602.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax", "600.0.0"..<"602.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.1.0"),
     ],
     targets: [
